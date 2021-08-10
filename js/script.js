103316dde100db30c9fab68dcb3ab88021b58813
bloom11 <bloomart11@gmail.com>
2021-08-10 17:46:29 -0400

    Added delimiter

--- js/script.js
+++ js/script.js
@@ -29,7 +29,9 @@ let remainingGuesses = 8;
 const getWord = async function () {
     const response = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
     const words = await response.text();
+    const wordArray = words.split("\n"); //Delimiter used to create array, so extra spacing around words is eliminated//
     console.log(words);
+    console.log(wordArray);
 };
 
 getWord()
